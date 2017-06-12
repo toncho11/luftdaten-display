a7f6a2c6a630665a40d9330fd1eec638facf49d1
toncho11@gmail.com <toncho11@gmail.com>
2017-06-12 19:42:48 +0200

    added handling of missing json files for the current day

--- script/utils.js
+++ script/utils.js
@@ -34,3 +34,23 @@ function filterByID(item) {
   //invalidEntries++;
   return false; 
 }
+
+function rsvpGetJSON(url) {
+	  var promise = new RSVP.Promise(function(resolve, reject){
+		var client = new XMLHttpRequest();
+		client.open("GET", url);
+		client.onreadystatechange = handler;
+		client.responseType = "json";
+		client.setRequestHeader("Accept", "application/json");
+		client.send();
+
+		function handler() {
+		  if (this.readyState === this.DONE) {
+			if (this.status === 200) { resolve(this.response); }
+			else { reject(this); }
+		  }
+		};
+	  });
+
+	  return promise;
+	};
